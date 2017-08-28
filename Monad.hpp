316b3175d6d9f86cdad4a22840b3d370dd134b61
Nikolas Vanderhoof <nvander1@binghamton.edu>
2017-08-28 16:22:35 -0400

    ClangFormat

--- Monad.hpp
+++ Monad.hpp
@@ -8,20 +8,18 @@
 // to bind.
 template <typename T>
 class Monad {
-public:
-    Monad(const T data) : data(data) {}
+ public:
+  Monad(const T data) : data(data) {}
 
-    template <typename F>
-    const Monad<T> bind(const F &func) const {
-      return func(this->data);
-    }
+  template <typename F>
+  const Monad<T> bind(const F& func) const {
+    return func(this->data);
+  }
 
-    const T peek() const {
-      return data;
-    }
+  const T peek() const { return data; }
 
-private:
-    const T data;
+ private:
+  const T data;
 };
 
 template <typename T>
