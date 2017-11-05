4aea678ab96750b8c5fffd0de29e62626f197da6
Nikolas Vanderhoof <nik@vanderhoof.pw>
2017-11-05 01:13:41 -0400

    Generate Maybe.hpp

--- Maybe.m4.hpp
+++ Maybe.m4.hpp
@@ -1,8 +1,9 @@
+include(`Monad.m4')dnl
 #pragma once
 
 #include <type_traits>
 
-#include "Monad.hpp"
+include(`Monad.hpp')
 
 namespace monad {
 
@@ -29,25 +30,11 @@ struct Just {
   const A d_val;
 };
 
-template <typename T>
-struct is_maybe_helper : std::false_type {};
-
-template <typename T>
-struct is_maybe_helper<Nothing<T>> : std::true_type {};
-
-template <typename T>
-struct is_maybe_helper<Just<T>> : std::true_type {};
-
-template <typename T>
-struct is_maybe
-    : is_maybe_helper<typename std::remove_reference_t<std::remove_cv_t<T>>> {};
-
-template <typename T>
-inline constexpr bool is_maybe_v = is_maybe<T>::value;
+is_monad_t(Maybe, Just, Nothing)
 
 template <typename Lhs, typename Rhs,
           typename = std::enable_if_t<is_maybe_v<Lhs> && is_maybe_v<Rhs>>>
-constexpr auto add(const Lhs &mx, const Rhs &my) {
+constexpr auto add(Lhs mx, Rhs my) {
   return mx >>= [=](const int x) constexpr {
     return my >>= [=](const int y) constexpr { return Just{x + y}; };
   };
