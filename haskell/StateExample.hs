422bb39b6d0e51b4d673792e27001769760005f8
Rushil Kumar <rukumar333@gmail.com>
2017-11-07 16:55:27 -0500

    Fixed up line breaks in presentation.

--- haskell/StateExample.hs
+++ haskell/StateExample.hs
@@ -3,17 +3,21 @@ import Data.List
 import Data.Functor.Identity
 
 removeMax :: (Num a, Eq a, Ord a) => State [a] a
-removeMax = StateT $ \list -> let maxNum = maximum list in Identity (maxNum, delete maxNum list)
+removeMax = StateT $
+            \list -> let maxNum = maximum list
+                     in Identity (maxNum, delete maxNum list)
 
-removeMaxThreeTimes :: (Num a, Eq a, Ord a) => State [a] (a, a, a)
+removeMaxThreeTimes ::
+  (Num a, Eq a, Ord a) => State [a] (a, a, a)
 removeMaxThreeTimes = do
   x <- removeMax
   y <- removeMax
   z <- removeMax
   return (x, y, z)
 
-removeMaxThreeTimesBind :: (Num a, Eq a, Ord a) => State [a] (a, a, a)
+removeMaxThreeTimesBind ::
+  (Num a, Eq a, Ord a) => State [a] (a, a, a)
 removeMaxThreeTimesBind = removeMax >>=
                           (\x -> removeMax >>=
-                                 (\y -> removeMax >>=
-                                        (\z -> return ((x, y, z)))))
+                            (\y -> removeMax >>=
+                              (\z -> return ((x, y, z)))))
