3e17c2dda0c7693e1de83072ed6a5500989af12c
Rushil Kumar <rukumar333@gmail.com>
2017-11-08 02:23:34 -0500

    Added new state example to haskell file.

--- haskell/StateExample.hs
+++ haskell/StateExample.hs
@@ -21,3 +21,21 @@ removeMaxThreeTimesBind = removeMax >>=
                           (\x -> removeMax >>=
                             (\y -> removeMax >>=
                               (\z -> return ((x, y, z)))))
+
+playString :: String -> State (Integer, Integer) Integer
+playString []     = do
+  (player1, player2) <- get
+  if player1 < player2 then return 2
+    else
+    if player2 < player1
+    then return 1
+    else return 0
+playString (x:xs) = do
+  (player1, player2) <- get
+  case x of
+    'a' -> put (player1 + 1, player2)
+    'b' -> put (player1 + 2, player2)
+    'c' -> put (player1, player2 + 1)
+    'd' -> put (player1, player2 + 2)
+    _   -> put (player1, player2)
+  playString xs
